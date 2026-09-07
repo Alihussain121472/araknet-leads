7df6d38999451bc14ec74680b92cc22639a85fbb
Syed Ali Hussain <217624331+Alihussain121472@users.noreply.github.com>
2026-09-07 22:02:05 +0500

    Fix next.config images deprecation warning

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["maps.googleapis.com", "images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
